a04b7045ddd124270c400b03c84fbff096b762dc
Alexandre Bonhomme <alexand.bonhom@gmail.com>
2016-01-11 22:35:27 +0100

    Implements copy task

--- gulpfile.js
+++ gulpfile.js
@@ -4,37 +4,61 @@ var gulp = require('gulp'),
     connect = require('gulp-connect'),
     watch = require('gulp-watch'),
     sass = require('gulp-sass'),
-    sourcemaps = require('gulp-sourcemaps');
+    sourcemaps = require('gulp-sourcemaps'),
+    browserify = require('gulp-browserify');
 
 gulp.task('connect', function() {
     connect.server({
+        root: 'src/',
         livereload: true
     });
 });
 
 gulp.task('livereload', function() {
     watch([
-        'css/*.css',
-        'js/**/*.js',
-        'img/**/*',
-        '**/*.html'
+        'src/css/*.css',
+        'src/js/*.js',
+        'src/img/**/*',
+        'src/**/*.html'
     ])
     .pipe(connect.reload());
 });
 
 gulp.task('sass', function () {
-    gulp.src('scss/**/*.scss')
+    gulp.src('src/scss/**/*.scss')
         .pipe(sourcemaps.init())
         .pipe(sass({
             outputStyle: 'expanded'
         }).on('error', sass.logError))
         .pipe(sourcemaps.write())
-        .pipe(gulp.dest('css/'));
+        .pipe(gulp.dest('./src/css'));
+});
+
+gulp.task('browserify', function () {
+    gulp.src('src/js/app.js')
+        .pipe(browserify({
+            insertGlobals: true,
+            debug: !gulp.env.production
+        }))
+        .pipe(gulp.dest('./src/js'));
 });
 
 gulp.task('watch', function () {
-    gulp.watch('scss/**/*.scss', ['sass']);
+    gulp.watch('src/scss/**/*.scss', ['sass']);
+    gulp.watch('src/js/**/*.js', ['browserify']);
 });
+
+gulp.task('copy', function () {
+    gulp.src([
+        'src/js/**/*',
+        'src/css/**/*',
+        'src/assets/**/*',
+        'src/index.html'
+    ], {
+        base: 'src/'
+    }).pipe(gulp.dest('dist/'));
+});
+
 
 // Usable tasks
 
